081d52719246b06178d828443b2828994d4184e4
misiekhardcore <misiekhardcore@gmail.com>
2021-05-27 12:53:30 +0200

    add BrowserRouter and Home route

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Home } from "./Home";
 
 interface RoutesProps {}
 
 const Routes: React.FC<RoutesProps> = () => {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home}></Route>
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 export default Routes;
