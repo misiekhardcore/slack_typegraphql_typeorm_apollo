61b2d83f9a28c61428f8b6317cd529e34dc8d0bb
misiekhardcore <misiekhardcore@gmail.com>
2021-08-16 21:58:42 +0200

    Create input types for channel

---
+++ server/src/inputs/ChannelInputs.ts
@@ -0,0 +1,25 @@
+import { Field, InputType } from "type-graphql";
+
+@InputType()
+export class CreateChannelInput {
+  @Field(() => String)
+  name: string;
+
+  @Field(() => Number)
+  teamId: number;
+
+  @Field(() => Boolean, { defaultValue: false })
+  isPublic?: boolean;
+}
+
+@InputType()
+export class UpdateChannelInput {
+  @Field(() => Number)
+  id: number;
+
+  @Field(() => String, { nullable: true })
+  name?: string;
+
+  @Field(() => Boolean, { defaultValue: false })
+  isPublic?: boolean;
+}
