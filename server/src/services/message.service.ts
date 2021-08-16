84211f3c5ec6b5d9d0c333c2447e9d9c1d11954b
misiekhardcore <misiekhardcore@gmail.com>
2021-08-16 22:09:25 +0200

    Create message service

---
+++ server/src/services/message.service.ts
@@ -0,0 +1,58 @@
+import { getRepository, Repository } from "typeorm";
+import { Message } from "../entity/Message";
+import {
+  CreateMessageInput,
+  UpdateMessageInput,
+} from "../inputs/MessageInput";
+
+export class MessageService {
+  private readonly messageRepository: Repository<Message>;
+  constructor() {
+    this.messageRepository = getRepository(Message);
+  }
+
+  public async create(
+    createMessageInput: CreateMessageInput,
+    userId: number
+  ) {
+    return await this.messageRepository
+      .create({ ...createMessageInput, userId })
+      .save();
+  }
+
+  public async update(updateMessageInput: UpdateMessageInput) {
+    const { id, ...rest } = updateMessageInput;
+    this.messageRepository.update({ id }, rest);
+    return await this.messageRepository.findOne(id);
+  }
+
+  public async getOne(id: number) {
+    return await this.messageRepository.findOne(id);
+  }
+
+  public async getMany() {
+    return await this.messageRepository.find();
+  }
+
+  public async populateMany<T>(
+    message: Message,
+    field: string
+  ): Promise<T[]> {
+    return await this.messageRepository
+      .createQueryBuilder()
+      .relation(Message, field)
+      .of(message)
+      .loadMany<T>();
+  }
+
+  public async populateOne<T>(
+    message: Message,
+    field: string
+  ): Promise<T | undefined> {
+    return await this.messageRepository
+      .createQueryBuilder()
+      .relation(Message, field)
+      .of(message)
+      .loadOne<T>();
+  }
+}
