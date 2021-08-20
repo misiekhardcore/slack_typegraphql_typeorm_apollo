250bccba19b953abd086c5058acd41499a0f9afc
misiekhardcore <misiekhardcore@gmail.com>
2021-08-20 21:32:24 +0200

    Add Login page

---
+++ client/src/routes/Login.tsx
@@ -0,0 +1,78 @@
+import { observer } from "mobx-react";
+import React, { useState } from "react";
+import { useHistory } from "react-router";
+import {
+  Button,
+  Container,
+  Form,
+  FormInput,
+  Header,
+  Message,
+} from "semantic-ui-react";
+import { useLoginMutation } from "../generated/graphql";
+import { graphqlErrorToObject } from "../utils/graphqlErrorToObject";
+import { isErrorField } from "../utils/isErrorField";
+
+export const Login = observer(() => {
+  const history = useHistory();
+  const [state, setState] = useState({ email: "", password: "" });
+  const [login, { loading, data, error }] = useLoginMutation({
+    errorPolicy: "all",
+  });
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setState({ ...state, [name]: value });
+  };
+
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    login({ variables: { loginUserInput: state } });
+  };
+
+  if (data?.login.ok) {
+    const { token, refreshToken } = data.login;
+    if (token) localStorage.setItem("token", token);
+    if (refreshToken)
+      localStorage.setItem("refreshToken", refreshToken);
+    history.push("/");
+  }
+
+  return (
+    <Container>
+      <Header as="h2">Login</Header>
+      {error && (
+        <>
+          <Message
+            error
+            list={graphqlErrorToObject(error)?.map((e) => e.message)}
+            header="You have errors in your form"
+          />
+        </>
+      )}
+      <Form onSubmit={onSubmit} loading={loading}>
+        <FormInput
+          error={isErrorField(error, "email")}
+          label="Email"
+          onChange={onChange}
+          name="email"
+          value={state.email}
+          fluid
+          placeholder="Email..."
+          type="email"
+        />
+        <FormInput
+          error={isErrorField(error, "password")}
+          label="Password"
+          onChange={onChange}
+          name="password"
+          value={state.password}
+          fluid
+          placeholder="Password..."
+          type="password"
+        />
+        <Button type="submit">Login</Button>
+      </Form>
+    </Container>
+  );
+});
--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -2,18 +2,16 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Home } from "./Home";
 import { Register } from "./Register";
+import { Login } from "./Login";
 
-interface RoutesProps {}
-
-const Routes: React.FC<RoutesProps> = () => {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Home}></Route>
-        <Route exact path="/register" component={Register}></Route>
-      </Switch>
-    </BrowserRouter>
-  );
-};
+const Routes: React.FC = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/register" component={Register} />
+      <Route exact path="/login" component={Login} />
+    </Switch>
+  </BrowserRouter>
+);
 
 export default Routes;
