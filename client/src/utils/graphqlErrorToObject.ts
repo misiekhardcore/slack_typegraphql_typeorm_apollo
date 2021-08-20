dd1e355060b127fe7ead1252d2972a40ea1b6dbb
misiekhardcore <misiekhardcore@gmail.com>
2021-08-20 21:34:06 +0200

    Fix typings

--- client/src/utils/graphqlErrorToObject.ts
+++ client/src/utils/graphqlErrorToObject.ts
@@ -1,10 +1,20 @@
 import { ApolloError } from "@apollo/client";
 
-export const graphqlErrorToObject = (error: ApolloError | undefined) => {
+export interface ErrorObject {
+  path: string;
+  message: string;
+}
+
+export const graphqlErrorToObject = (
+  error: ApolloError | undefined
+): ErrorObject[] => {
   if (!error) return [];
+
   const { graphQLErrors } = error;
-  if (!graphQLErrors[0]) throw error;
+
+  if (!graphQLErrors[0]) return [];
   const { extensions } = graphQLErrors[0];
+
   return extensions?.exception?.validationErrors?.map((a: any) => {
     const keys = Object.keys(a.constraints)[0];
     return { path: a.property, message: a.constraints[keys] };
