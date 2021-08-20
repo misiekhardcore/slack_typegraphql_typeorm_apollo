da92cda63219bedd6c6dbc22e751d1c172506579
misiekhardcore <misiekhardcore@gmail.com>
2021-08-20 21:33:16 +0200

    Create function to look for a field in errors

---
+++ client/src/utils/isErrorField.ts
@@ -0,0 +1,13 @@
+import { ApolloError } from "@apollo/client";
+import { graphqlErrorToObject } from "./graphqlErrorToObject";
+
+export const isErrorField = (
+  error: ApolloError | undefined,
+  name: string
+): boolean => {
+  return graphqlErrorToObject(error)?.find(
+    (error) => error.path === name
+  )
+    ? true
+    : false;
+};
