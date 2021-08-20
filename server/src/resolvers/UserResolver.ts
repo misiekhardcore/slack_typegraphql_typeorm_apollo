c918132deffec2ed414fca2675f36a4e371dffd5
misiekhardcore <misiekhardcore@gmail.com>
2021-08-20 21:36:51 +0200

    Add Login mutation

--- server/src/resolvers/UserResolver.ts
+++ server/src/resolvers/UserResolver.ts
@@ -1,6 +1,7 @@
-import argon2 from "argon2";
+import { CreateUserResponse, LoginResponse } from "../entity/Outputs";
 import {
   Arg,
+  Ctx,
   FieldResolver,
   Mutation,
   Query,
@@ -11,8 +12,13 @@ import {
 import { Channel } from "../entity/Channel";
 import { Team } from "../entity/Team";
 import { User } from "../entity/User";
-import { CreateUserInput, UpdateUserInput } from "../inputs/UserInputs";
+import {
+  CreateUserInput,
+  LoginUserInput,
+  UpdateUserInput,
+} from "../inputs/UserInputs";
 import { UserService } from "../services/user.service";
+import { Context } from "../index";
 
 @Resolver(() => User)
 export class UserResolver implements ResolverInterface<User> {
@@ -21,29 +27,60 @@ export class UserResolver implements ResolverInterface<User> {
     this.userService = new UserService();
   }
   @Query(() => [User], { nullable: true })
-  async getUsers() {
+  async getUsers(): Promise<User[]> {
     return await this.userService.getMany();
   }
 
   @Query(() => User, { nullable: true })
-  async getUser(@Arg("id") id: number) {
-    return await this.userService.getOne(id);
+  async getUser(@Arg("id") id: number): Promise<User | null> {
+    return (await this.userService.getOneById(id)) || null;
   }
 
-  @Mutation(() => User)
+  @Mutation(() => CreateUserResponse)
   async register(
     @Arg("userInput")
-    { password, ...restArgs }: CreateUserInput
-  ) {
+    cerateUserInput: CreateUserInput
+  ): Promise<CreateUserResponse> {
     try {
-      const hashPassword = await argon2.hash(password);
-      return await this.userService.create({
-        ...restArgs,
-        password: hashPassword,
-      });
+      const user = await this.userService.create(cerateUserInput);
+      return { ok: true, user };
     } catch (error) {
-      throw new Error(error);
+      return { ok: false, errors: error };
     }
+  }
+
+  @Mutation(() => LoginResponse)
+  async login(
+    @Arg("userInput") loginUserInput: LoginUserInput,
+    @Ctx() { jwtSecret1, jwtSecret2 }: Context
+  ): Promise<LoginResponse> {
+    const { email, password } = loginUserInput;
+    const user = await this.userService.getOneByEmail(email);
+    if (!user)
+      return {
+        ok: false,
+        errors: [{ path: "email", msg: "user not found" }],
+      };
+    if (!user.validatePassword(password))
+      return {
+        ok: false,
+        errors: [
+          { path: "email", msg: "invalid email or password" },
+          { path: "password", msg: "" },
+        ],
+      };
+
+    const refreshTokenSecret = user.password + jwtSecret2;
+
+    const [token, refreshToken] = user.createTokens(
+      jwtSecret1,
+      refreshTokenSecret
+    );
+    return {
+      ok: true,
+      token,
+      refreshToken,
+    };
   }
 
   @Mutation(() => User)
