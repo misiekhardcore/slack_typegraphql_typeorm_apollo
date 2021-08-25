ae50fbb6f03cf97d64ee4fae6b03ce8e10896b53
misiekhardcore <misiekhardcore@gmail.com>
2021-08-25 19:46:29 +0200

    Add extractTokens functionality

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,26 +1,73 @@
 import { ApolloServer } from "apollo-server-express";
-import express from "express";
+import { config } from "dotenv";
+import express, { Request, Response } from "express";
+import jwt from "jsonwebtoken";
 import "reflect-metadata";
 import { buildSchema } from "type-graphql";
 import { createConnection, getConnectionOptions } from "typeorm";
 import { SnakeNamingStrategy } from "typeorm-naming-strategies";
+import { JWTTokenPayload, User } from "./entity/User";
 import { ChannelResolver } from "./resolvers/ChannelResolver";
 import { MessageResolver } from "./resolvers/MessageResolver";
 import { TeamResolver } from "./resolvers/TeamResolver";
 import { UserResolver } from "./resolvers/UserResolver";
-import { config } from "dotenv";
 
 config();
 
 export interface Context {
   req: any;
   res: any;
-  user: { id: number };
+  user: { id: number } | null;
   jwtSecret1: string;
   jwtSecret2: string;
 }
 (async () => {
   const app = express();
+
+  /**
+   * Extracts the user info from header tokens and returns it. Creates new tkens and put them in response header.
+   * @param {Request} req express request
+   * @param {Response} res express response
+   * @returns user info
+   */
+  const extractTokens = async (
+    req: Request<any>,
+    res: Response<any>
+  ) => {
+    const token = req.headers["x-token"] as string;
+    if (token) {
+      try {
+        const { user } = jwt.verify(
+          token,
+          process.env.SECRET1 || ""
+        ) as JWTTokenPayload;
+        return user;
+      } catch (error) {
+        const refreshToken =
+          (req.headers["x-refresh-token"] as string) || "";
+
+        const newTokens = await new User().refreshTokens(
+          refreshToken,
+          process.env.SECRET1 || "",
+          process.env.SECRET2 || ""
+        );
+
+        if (!newTokens) return undefined;
+
+        if (newTokens.token && newTokens.refreshToken) {
+          res.setHeader(
+            "Access-Control-Expose-Headers",
+            "x-token, x-refresh-token"
+          );
+
+          res.setHeader("x-token", newTokens.token);
+          res.setHeader("x-refresh-token", newTokens.refreshToken);
+        }
+        return newTokens.user;
+      }
+    }
+    return undefined;
+  };
 
   const options = await getConnectionOptions(
     process.env.NODE_ENV || "production"
@@ -41,13 +88,15 @@ export interface Context {
       ],
       validate: true,
     }),
-    context: ({ req, res }): Context => ({
-      req,
-      res,
-      user: { id: 1 },
-      jwtSecret1: process.env.SECRET1 || "supersecret",
-      jwtSecret2: process.env.SECRET2 || "supersecret",
-    }),
+    context: async ({ req, res }): Promise<Context> => {
+      return {
+        req,
+        res,
+        user: (await extractTokens(req, res)) || null,
+        jwtSecret1: process.env.SECRET1 || "supersecret",
+        jwtSecret2: process.env.SECRET2 || "supersecret",
+      };
+    },
   });
 
   const corsOptions = {
