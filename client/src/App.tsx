dc1d5b9ba1aeaec7ac4ed7cc4a8b6182bab9b8da
misiekhardcore <misiekhardcore@gmail.com>
2021-08-25 19:24:40 +0200

    Add auth middleware

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,12 +6,29 @@ import {
 } from "@apollo/client";
 import React from "react";
 import Routes from "./routes";
+import { setContext } from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:4000/graphql",
+  credentials: "same-origin",
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem("token");
+  const refreshToken = localStorage.getItem("refreshToken");
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      "x-token": token ? `${token}` : "",
+      "x-refresh-token": refreshToken ? `${refreshToken}` : "",
+    },
+  };
+});
 
 const client = new ApolloClient({
-  link: createHttpLink({
-    uri: "http://localhost:4000/graphql",
-    credentials: "same-origin",
-  }),
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
