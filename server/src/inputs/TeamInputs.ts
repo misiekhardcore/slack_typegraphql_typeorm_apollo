8862154c76fb24706d77f46b447201d952b7c9f4
misiekhardcore <misiekhardcore@gmail.com>
2021-08-25 19:30:52 +0200

    Add TeamInput validation

--- server/src/inputs/TeamInputs.ts
+++ server/src/inputs/TeamInputs.ts
@@ -1,8 +1,12 @@
+import { Length } from "class-validator";
 import { Field, InputType } from "type-graphql";
 
 @InputType()
 export class CreateTeamInput {
   @Field(() => String)
+  @Length(3, 30, {
+    message: "Team name has to be between 3 and 30 characters",
+  })
   name: string;
 }
 
