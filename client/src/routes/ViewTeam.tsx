f1e0e0dfeae0f1f167a9aa4ac84e0a7918ec98a5
misiekhardcore <misiekhardcore@gmail.com>
2021-08-26 21:34:19 +0200

    Move some logic to Sidebar component

--- client/src/routes/ViewTeam.tsx
+++ client/src/routes/ViewTeam.tsx
@@ -1,26 +1,24 @@
 import React from "react";
 import { AppLayout } from "../components/AppLayout";
-import { Channels } from "../components/Channels";
 import { Header } from "../components/Header";
-import { Input } from "../components/Input";
 import { Messages } from "../components/Messages";
-import { Teams } from "../components/Teams";
+import { SendMessage } from "../components/SendMessage";
+import { Sidebar } from "../containers/Sidebar";
 
 export const ViewTeam: React.FC = () => {
   return (
     <AppLayout>
-      <Teams>Teams</Teams>
-      <Channels>Channels</Channels>
-      <Header>Header</Header>
+      <Sidebar currentTeamId={9} />
+      <Header channelName="channel1" />
       <Messages>
         <ul className="message-list">
           <li></li>
           <li></li>
         </ul>
       </Messages>
-      <Input>
+      <SendMessage channelName="channel1">
         <input type="text" placeholder="CSS Grid Layout Module" />
-      </Input>
+      </SendMessage>
     </AppLayout>
   );
 };
