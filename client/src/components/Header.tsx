87d0107d41a70bf87ff8076e8cfc46722334d6e8
misiekhardcore <misiekhardcore@gmail.com>
2021-08-26 21:34:00 +0200

    Transform from styled to react component

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,7 +1,20 @@
 import styled from "styled-components";
+import { Header as He } from "semantic-ui-react";
 
-export const Header = styled.div`
+export const HeaderWrapper = styled.div`
   grid-column: 3;
   grid-row: 1;
   /* background-color: #4e3a4c; */
 `;
+
+interface HeaderPros {
+  channelName: string;
+}
+
+export const Header: React.FC<HeaderPros> = ({ channelName }) => {
+  return (
+    <HeaderWrapper>
+      <He>{channelName}</He>
+    </HeaderWrapper>
+  );
+};
--- client/src/components/Teams.tsx
+++ client/src/components/Teams.tsx
@@ -1,7 +1,49 @@
+import React from "react";
 import styled from "styled-components";
 
-export const Teams = styled.div`
+const TeamsWrapper = styled.div`
   grid-column: 1;
   grid-row: 1/4;
   background-color: #362234;
+  color: #958993;
 `;
+
+const TeamList = styled.ul`
+  width: 100%;
+  padding-left: 0px;
+  list-style: none;
+`;
+
+const TeamListItem = styled.li`
+  height: 50px;
+  width: 50px;
+  background-color: #676066;
+  color: #fff;
+  margin: auto;
+  margin-bottom: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 24px;
+  border-radius: 11px;
+  &:hover {
+    border: thick solid #767676;
+  }
+`;
+
+type TTeam = { id: number; letter: string };
+
+const team = ({ id, letter }: TTeam): JSX.Element => (
+  <TeamListItem key={`team-${id}`}>{letter}</TeamListItem>
+);
+interface TeamsProps {
+  teams: TTeam[];
+}
+
+export const Teams: React.FC<TeamsProps> = ({ teams }) => {
+  return (
+    <TeamsWrapper>
+      <TeamList>{teams.map(team)}</TeamList>
+    </TeamsWrapper>
+  );
+};
