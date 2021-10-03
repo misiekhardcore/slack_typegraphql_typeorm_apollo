baaf0139a55b3f4df3037da4d8112091382eb414
misiekhardcore <misiekhardcore@gmail.com>
2021-10-03 15:26:02 +0200

    Create function to change server error format

---
+++ client/src/utils/errorToFieldError.ts
@@ -0,0 +1,13 @@
+import { ListError } from "../generated/graphql";
+
+export default function errorToFieldError(errors: ListError[]) {
+  if (!errors || errors.length === 0) return {};
+  return errors.reduce((acc: any, error) => {
+    if (error.path in acc) {
+      acc[error.path].push(error.msg);
+    } else {
+      acc[error.path] = [error.msg];
+    }
+    return acc;
+  }, {});
+}
