188c7c405d0d21067ba5fc71d1af285dd6edeed0
misiekhardcore <misiekhardcore@gmail.com>
2021-10-03 15:20:39 +0200

    Add modals handling

--- client/src/containers/Sidebar.tsx
+++ client/src/containers/Sidebar.tsx
@@ -1,49 +1,77 @@
-import _ from "lodash";
-import React from "react";
 import decode from "jwt-decode";
+import React, { useState } from "react";
+import { AddChannelModal } from "../components/AddChannelModal";
+import { InvitePeopleModal } from "../components/InvitePeopleModal";
 import { Channels } from "../components/Channels";
 import { Teams } from "../components/Teams";
-import { useGetTeamsQuery } from "../generated/graphql";
+import { Team } from "../generated/graphql";
 import { JWTTokenPayload } from "../types";
+import { useHistory } from "react-router";
 
 interface SidebarProps {
-  currentTeamId: number;
+  teams: { id: number; letter: string }[];
+  team: Team;
 }
 
-export const Sidebar: React.FC<SidebarProps> = ({ currentTeamId }) => {
-  const { loading, error, data } = useGetTeamsQuery();
-  const teams = data?.getTeams;
-  if (!teams) return null;
-  const teamIdx = _.findIndex(teams, ["id", currentTeamId]);
-  const team = teams[teamIdx];
-  const channels = team.channels;
+export const Sidebar: React.FC<SidebarProps> = ({ teams, team }) => {
+  const history = useHistory();
+  const [state, setState] = useState({
+    openAddChannelModal: false,
+    openInvitePeopleModal: false,
+  });
+  if (!(teams && team)) {
+    history.push("/login");
+    return null;
+  }
+
+  const channels = team?.channels || [];
 
   let username = "";
   try {
     const token = localStorage.getItem("token") || "";
+    if (!token) history.push("/login");
     const { user } = decode(token) as JWTTokenPayload;
+    if (!user) history.push("/login");
     username = user.username;
   } catch (error) {}
 
-  return loading || error ? null : (
+  const handleAddChannelModalClose = () =>
+    setState({ ...state, openAddChannelModal: false });
+
+  const handleAddChannelModalOpen = () =>
+    setState({ ...state, openAddChannelModal: true });
+
+  const handleInvitePeopleModalClose = () =>
+    setState({ ...state, openInvitePeopleModal: false });
+
+  const handleInvitePeopleModalOpen = () =>
+    setState({ ...state, openInvitePeopleModal: true });
+
+  return (
     <>
-      <Teams
-        key="team-sidebar"
-        teams={teams.map((team) => ({
-          ...team,
-          id: Number(team.id),
-          letter: team.name.slice(0, 1).toUpperCase(),
-        }))}
-      />
+      <Teams key="team-sidebar" teams={teams} />
       <Channels
         key="channel-sidebar"
-        teamName={team.name}
+        teamId={team.id}
+        teamName={team?.name}
         userName={username}
         channels={channels}
         users={[
           { id: 1, name: "user1" },
           { id: 2, name: "user2" },
         ]}
+        onAddChannelClick={handleAddChannelModalOpen}
+        onInvitePeople={handleInvitePeopleModalOpen}
+      />
+      <AddChannelModal
+        teamId={team.id}
+        open={state.openAddChannelModal}
+        onClose={handleAddChannelModalClose}
+      />
+      <InvitePeopleModal
+        teamId={team.id}
+        open={state.openInvitePeopleModal}
+        onClose={handleInvitePeopleModalClose}
       />
     </>
   );
