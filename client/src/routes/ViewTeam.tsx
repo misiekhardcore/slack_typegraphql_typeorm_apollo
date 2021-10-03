d87cb3f52dc12b5037ddcbfbb5a35aaf753e42be
misiekhardcore <misiekhardcore@gmail.com>
2021-10-03 15:24:50 +0200

    Use real server teams data

--- client/src/routes/ViewTeam.tsx
+++ client/src/routes/ViewTeam.tsx
@@ -1,24 +1,64 @@
+import { findIndex } from "lodash";
 import React from "react";
+import { useHistory } from "react-router";
 import { AppLayout } from "../components/AppLayout";
 import { Header } from "../components/Header";
 import { Messages } from "../components/Messages";
 import { SendMessage } from "../components/SendMessage";
 import { Sidebar } from "../containers/Sidebar";
+import { Team, useGetTeamsQuery } from "../generated/graphql";
 
-export const ViewTeam: React.FC = () => {
+interface ViewTeamProps {
+  match: { params: { teamId: string; channelId: string } };
+}
+
+const ViewTeam: React.FC<ViewTeamProps> = ({
+  match: {
+    params: { teamId, channelId },
+  },
+}) => {
+  const history = useHistory();
+  const { loading, error, data } = useGetTeamsQuery();
+
+  const { getTeams } = data || {};
+  if (!getTeams) return null;
+
+  const teamIdx = teamId ? findIndex(getTeams, ["id", +teamId]) : 0;
+  const team = getTeams[teamIdx];
+
+  if (!team) {
+    history.push("/login");
+    return null;
+  }
+
+  const channelIdx = channelId
+    ? findIndex(team.channels, ["id", +channelId])
+    : 0;
+  const channel = team?.channels[channelIdx] || {};
+
+  if (loading || error) return null;
+
   return (
     <AppLayout>
-      <Sidebar currentTeamId={9} />
-      <Header channelName="channel1" />
+      <Sidebar
+        teams={getTeams.map((t) => ({
+          id: t.id,
+          letter: t.name.charAt(0).toUpperCase(),
+        }))}
+        team={team as Team}
+      />
+      <Header channelName={channel?.name || "none"} />
       <Messages>
         <ul className="message-list">
           <li></li>
           <li></li>
         </ul>
       </Messages>
-      <SendMessage channelName="channel1">
+      <SendMessage channelName="general">
         <input type="text" placeholder="CSS Grid Layout Module" />
       </SendMessage>
     </AppLayout>
   );
 };
+
+export default ViewTeam;
