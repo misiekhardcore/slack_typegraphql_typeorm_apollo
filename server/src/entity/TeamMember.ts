2077f32ae2a4f0e27bc56bbd22ef19f74a2c5065
misiekhardcore <misiekhardcore@gmail.com>
2021-10-03 15:28:01 +0200

    Create join table for Team-User

---
+++ server/src/entity/TeamMember.ts
@@ -0,0 +1,38 @@
+import {
+  BaseEntity,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { Team } from "./Team";
+import { User } from "./User";
+
+@Entity({ name: "team_member" })
+export class TeamMember extends BaseEntity {
+  @PrimaryColumn()
+  userId: number;
+
+  @PrimaryColumn()
+  teamId: number;
+
+  @ManyToOne(() => User, (user) => user.teamConnection, {
+    primary: true,
+  })
+  @JoinColumn({ name: "user_id" })
+  user: Promise<User>;
+
+  @ManyToOne(() => Team, (team) => team.userConnection, {
+    primary: true,
+  })
+  @JoinColumn({ name: "team_id" })
+  team: Promise<Team>;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
