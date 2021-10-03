dc1931cee3934afa2f43cee7366125f5057b6bbf
misiekhardcore <misiekhardcore@gmail.com>
2021-10-03 21:55:00 +0200

    Add ownership check

--- server/src/resolvers/ChannelResolver.ts
+++ server/src/resolvers/ChannelResolver.ts
@@ -1,24 +1,31 @@
 import {
   Arg,
+  Ctx,
   FieldResolver,
   Mutation,
   Query,
   Resolver,
   ResolverInterface,
   Root,
+  UseMiddleware,
 } from "type-graphql";
 import { Channel } from "../entity/Channel";
 import { Message } from "../entity/Message";
 import { CreateChannelResponse } from "../entity/Outputs";
 import { Team } from "../entity/Team";
+import { Context } from "../index";
 import { CreateChannelInput } from "../inputs/ChannelInputs";
+import { isAuth } from "../permissions";
 import { ChannelService } from "../services/channel.service";
+import { TeamService } from "../services/team.service";
 
 @Resolver(() => Channel)
 export class ChannelResolver implements ResolverInterface<Channel> {
   private readonly channelService: ChannelService;
+  private readonly teamService: TeamService;
   constructor() {
     this.channelService = new ChannelService();
+    this.teamService = new TeamService();
   }
 
   @Query(() => [Channel], { nullable: true })
@@ -32,13 +39,31 @@ export class ChannelResolver implements ResolverInterface<Channel> {
   }
 
   @Mutation(() => CreateChannelResponse)
+  @UseMiddleware(isAuth)
   async createChannel(
-    @Arg("channelInput") createChannelInput: CreateChannelInput
+    @Arg("channelInput") createChannelInput: CreateChannelInput,
+    @Ctx() { user }: Context
   ): Promise<CreateChannelResponse> {
     try {
+      const team = await this.teamService.getOne(
+        createChannelInput.teamId
+      );
+
+      if (team?.ownerId !== user?.id)
+        return {
+          ok: false,
+          errors: [
+            {
+              msg: "You have to be team owner to create channels",
+              path: "channelName",
+            },
+          ],
+        };
+
       const channel = await this.channelService.create(
         createChannelInput
       );
+
       return { ok: true, channel };
     } catch (error) {
       return {
