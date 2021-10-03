fb30f6ac419eb7f7e323a7ae89ef50eaded10a56
misiekhardcore <misiekhardcore@gmail.com>
2021-10-03 15:27:48 +0200

    Create join table for Channel-User

---
+++ server/src/entity/ChannelMember.ts
@@ -0,0 +1,38 @@
+import {
+  BaseEntity,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { Channel } from "./Channel";
+import { User } from "./User";
+
+@Entity({ name: "channel_user" })
+export class ChannelMember extends BaseEntity {
+  @PrimaryColumn()
+  userId: number;
+
+  @PrimaryColumn()
+  channelId: number;
+
+  @ManyToOne(() => User, (user) => user.channelConnection, {
+    primary: true,
+  })
+  @JoinColumn({ name: "user_id" })
+  user: User;
+
+  @ManyToOne(() => Channel, (channel) => channel.userConnection, {
+    primary: true,
+  })
+  @JoinColumn({ name: "channel_id" })
+  channel: Channel;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
