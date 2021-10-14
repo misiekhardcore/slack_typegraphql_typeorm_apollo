e02c3ef72eb28af0fa6aab00aa2d057b3f985880
misiekhardcore <misiekhardcore@gmail.com>
2021-10-14 22:45:59 +0200

    Add background color

--- client/src/components/AppLayout.tsx
+++ client/src/components/AppLayout.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const AppLayout = styled.div`
   display: grid;
   height: 100vh;
+  background-color: #c2b7d6;
   grid-template-columns: 100px 250px 1fr;
   grid-template-rows: auto 1fr auto;
 `;
