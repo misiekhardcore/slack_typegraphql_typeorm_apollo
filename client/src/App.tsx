ce6a1a4649b7c5cdc4a4650c853c19a4a6427c26
misiekhardcore <misiekhardcore@gmail.com>
2021-10-14 22:45:23 +0200

    Move Apolloo logic to separate file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,36 +1,7 @@
-import {
-  ApolloClient,
-  ApolloProvider,
-  createHttpLink,
-  InMemoryCache,
-} from "@apollo/client";
 import React from "react";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "./apollo";
 import Routes from "./routes";
-import { setContext } from "@apollo/client/link/context";
-
-const httpLink = createHttpLink({
-  uri: "http://localhost:4000/graphql",
-  credentials: "same-origin",
-});
-
-const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem("token");
-  const refreshToken = localStorage.getItem("refreshToken");
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      "x-token": token ? `${token}` : "",
-      "x-refresh-token": refreshToken ? `${refreshToken}` : "",
-    },
-  };
-});
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
 
 const App = (): JSX.Element => (
   <ApolloProvider client={client}>
---
+++ client/src/apollo.ts
@@ -0,0 +1,52 @@
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+  split,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { getMainDefinition } from "@apollo/client/utilities";
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem("token");
+  const refreshToken = localStorage.getItem("refreshToken");
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      "x-token": token ? `${token}` : "",
+      "x-refresh-token": refreshToken ? `${refreshToken}` : "",
+    },
+  };
+});
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:4000/graphql",
+  credentials: "same-origin",
+});
+
+const wsLink = new WebSocketLink({
+  uri: "ws://localhost:4000/subscriptions",
+  options: {
+    reconnect: true,
+  },
+});
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  authLink.concat(httpLink)
+);
+
+export const client = new ApolloClient({
+  link: splitLink,
+  cache: new InMemoryCache(),
+});
