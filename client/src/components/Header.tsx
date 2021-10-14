8bf9ce84e888f177a96da9a25e1b976ef31cd594
misiekhardcore <misiekhardcore@gmail.com>
2021-10-14 22:46:18 +0200

    Add background color

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,10 +2,12 @@ import styled from "styled-components";
 import { Header as He } from "semantic-ui-react";
 
 export const HeaderWrapper = styled.div`
+  width: 100%;
+  padding: 1.5rem;
   grid-column: 3;
   grid-row: 1;
-  justify-self: center;
-  /* background-color: #4e3a4c; */
+  text-align: center;
+  background-color: #b9a7c5;
 `;
 
 interface HeaderPros {
